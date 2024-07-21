82285d38852d01f854bb6fe3342adb612cab9cfa
Gouravsharma20 <gouravsharma20a@gmail.com>
2024-07-21 21:17:53 +0530

    we have added apis and all

---
+++ composeApp/src/commonMain/kotlin/data/remote/api/CurrencyApiService.kt
@@ -0,0 +1,8 @@
+package data.remote.api
+
+class CurrencyApiService {
+    companion object {
+        const val ENDPOINT = " cur_live_Ah9RGPXAdKgKjq22W1jUPxhMS4n2IrdhQfpIkyNF"
+        const val API_KEY = " https://api.currencyapi.com/v3/latest"
+    }
+}
